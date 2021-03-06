2e9c63e63b6cf1e910947c66d61d3031fb3c904e
Emmanuel Brito <britoemmanuel98@gmail.com>
2021-03-05 23:17:11 -0400

    add customer module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./modules/customer/customer.module').then(m => m.CustomerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
